bd881571024f7b227214fd536424fc011ff4fd1a
Gabriel <gabriel@pop-os.localdomain>
2022-10-21 18:36:53 -0300

    imprimindo media dos valores

--- Fundamentos/secao-04-introducao-a-javascript/2-javascript-array-e-loop-for/Lidando-com-Arrays.js
+++ Fundamentos/secao-04-introducao-a-javascript/2-javascript-array-e-loop-for/Lidando-com-Arrays.js
@@ -7,3 +7,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0 ; index < numbers.length; index += 1) {
     console.log(numbers[index]);
 }
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+let soma = 0;
+let espaco = " ";
+
+for (let index = 0 ; index < numbers.length; index += 1) {
+   soma += numbers[index];
+}
+
+console.log('a soma dos valores é: ' + soma);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+
+let media = 0;
+
+for (let index = 0 ; index < numbers.length; index += 1) {
+    media += numbers[index] / numbers.length;
+}
+
+console.log('a media dos valores é: ' + media)
