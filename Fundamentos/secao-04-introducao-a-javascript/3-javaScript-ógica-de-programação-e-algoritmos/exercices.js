a37d5c40d29c1b8743f254606d5c9deef2c1ee7c
Gabriel <gabriel@pop-os.localdomain>
2022-10-24 19:54:07 -0300

    menor e maior palavra do array

--- Fundamentos/secao-04-introducao-a-javascript/3-javaScript-ógica-de-programação-e-algoritmos/exercices.js
+++ Fundamentos/secao-04-introducao-a-javascript/3-javaScript-ógica-de-programação-e-algoritmos/exercices.js
@@ -12,3 +12,41 @@ for (index = 1; index <= 10; index +=1) {
 }
 
 console.log(fatorial);
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'banana';
+let reverse = '';
+
+for (index = 0; index < word.length; index += 1) {
+    reverse += word[word.length - 1 - index];;
+}
+
+console.log(reverse);
+
+// Considere o array de strings abaixo:
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array [0];
+let menorPalavra = array [0];
+
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > maiorPalavra.length){
+        maiorPalavra = array[index];
+    }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length < menorPalavra.length){
+        menorPalavra = array[index];
+    }
+    
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
