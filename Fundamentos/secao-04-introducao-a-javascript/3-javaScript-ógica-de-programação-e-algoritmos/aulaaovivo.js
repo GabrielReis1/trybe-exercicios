de8affad4c5621bccf9e1bb8cebc2832dc071a59
Gabriel <gabriel@pop-os.localdomain>
2022-10-24 18:28:30 -0300

    fatorial de 10

---
+++ Fundamentos/secao-04-introducao-a-javascript/3-javaScript-ógica-de-programação-e-algoritmos/aulaaovivo.js
@@ -0,0 +1,42 @@
+// 1 - Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+// A soma total de 1 a 50 é: X
+
+let soma = 0;
+
+for (let index = 1; index <= 50; index += 1) {
+    soma += index;
+}
+
+console.log(" A soma total de 1 a 50 é:", soma);
+
+// 2 - Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
+
+let cont = 0;
+
+for(let i = 2; i <= 150; i += 1) {
+    if (i % 3 === 0){
+        cont += 1
+    }
+}
+
+if (cont === 50) {
+    console.log("mensagem secreta")
+}
+
+// 5 - Crie um algoritmo que recebe a idade de Carolzita, Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
+
+let Carolzita = 25;
+let Murilo = 18;
+let Baêta = 30;
+
+if(Carolzita < Murilo && Carolzita < Baêta) {
+    console.log('Carolzita é a mais nova!');
+} else if (Murilo < Carolzita && Murilo < Baêta) {
+    console.log('Murilo é o mais novo');
+}else if (Baêta < Carolzita && Baêta < Murilo) {
+    console.log('Baêta é a mais nova!');
+} else {
+    console.log('Todos tem a mesma idade!')
+}
--- Fundamentos/secao-04-introducao-a-javascript/3-javaScript-ógica-de-programação-e-algoritmos/exercices.js
+++ Fundamentos/secao-04-introducao-a-javascript/3-javaScript-ógica-de-programação-e-algoritmos/exercices.js
@@ -1,42 +1,14 @@
-// 1 - Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
-// A soma total de 1 a 50 é: X
+// O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
 
-let soma = 0;
+// O fatorial é representado pelo sinal !
+// 4! = 4 x 3 x 2 x 1 = 24
 
-for (let index = 1; index <= 50; index += 1) {
-    soma += index;
+// Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+let fatorial = 10;
+
+for (index = 1; index <= 10; index +=1) {
+    fatorial *= index;
 }
 
-console.log(" A soma total de 1 a 50 é:", soma);
-
-// 2 - Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3.
-// Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
-
-let cont = 0;
-
-for(let i = 2; i <= 150; i += 1) {
-    if (i % 3 === 0){
-        cont += 1
-    }
-}
-
-if (cont === 50) {
-    console.log("mensagem secreta")
-}
-
-// 5 - Crie um algoritmo que recebe a idade de Carolzita, Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
-// "Pessoa" é a mais nova.
-
-let Carolzita = 25;
-let Murilo = 18;
-let Baêta = 30;
-
-if(Carolzita < Murilo && Carolzita < Baêta) {
-    console.log('Carolzita é a mais nova!');
-} else if (Murilo < Carolzita && Murilo < Baêta) {
-    console.log('Murilo é o mais novo');
-}else if (Baêta < Carolzita && Baêta < Murilo) {
-    console.log('Baêta é a mais nova!');
-} else {
-    console.log('Todos tem a mesma idade!')
-}
+console.log(fatorial);
