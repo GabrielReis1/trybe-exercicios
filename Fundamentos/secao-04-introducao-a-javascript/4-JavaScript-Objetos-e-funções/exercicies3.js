63d2c8e6a4b209e5799ac6a99b6056f3fa36f15e
Gabriel <gabriel@pop-os.localdomain>
2022-10-25 18:38:35 -0300

    fazendo cindo programas para cada operação aritimetica basica

---
+++ Fundamentos/secao-04-introducao-a-javascript/4-JavaScript-Objetos-e-funções/exercicies3.js
@@ -0,0 +1,33 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function subtraction(num1, num2) {
+    return num1 - num2
+}
+
+function multiplication(num1, num2) {
+    return num1 * num2
+}
+
+function division(num1, num2) {
+    return num1 / num2
+}
+
+function module(num1, num2) {
+    return num1 % num2
+}
+
+console.log(sum(4, 6))
+console.log(subtraction(13, 5))
+console.log(multiplication(9, 7))
+console.log(division(24, 6))
+console.log(module(32, 3))
