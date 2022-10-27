9e09b379d9e5efea6a362cbc402b03ccef86485e
Gabriel <gabriel@pop-os.localdomain>
2022-10-27 08:21:46 -0300

    if/else programa que retorna maior de dois numeros

---
+++ Fundamentos/secao-04-introducao-a-javascript/1-javascript-primeiros-passos/exercises2.js
@@ -0,0 +1,11 @@
+// Utilize if/else para fazer um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+
+
+const num1 = 50;
+const num2 = 25;
+
+if(num1 > num2) {
+    console.log('o maoir numero é: ', num1);
+} else {
+    console.log('o maior numero é: ', num2);
+}
